c7783724451fb2048899b5df94edb6f91fce882a
Atvars Trams <atvars.trams@fizmati.lv>
2021-09-13 17:36:51 +0300

    2tasks

--- Projects2/ConsoleHomeWork2/ConsoleHomeWork2/Program.cs
+++ Projects2/ConsoleHomeWork2/ConsoleHomeWork2/Program.cs
@@ -6,10 +6,20 @@ namespace ConsoleHomeWork2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ievadi savu vārdu");
-            string userAnswer = Console.ReadLine();
+            Console.WriteLine("Kā Tevi sauc?");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Sveiks, {userName}");
+            Console.WriteLine("=============================================================");
 
-            Console.WriteLine("Hi! " + userAnswer);
+            Console.WriteLine("Kāds ir Tavs vecums?");
+            string userAge = Console.ReadLine();
+
+            int age = int.Parse(userAge);
+            int ageNextYear = age + 1;
+            bool Is18 = age >= 18;
+
+            Console.WriteLine($"Nākamgad Tev paliks {ageNextYear}, Tu esi pilngadīgs:  {Is18}");
+
 
         }
     }
